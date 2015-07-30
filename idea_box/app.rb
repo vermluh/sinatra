1fb9d76f016a3d4b5482455c82ff9b1d283a4b4a
Florian Simon <github@verm-luh.de>
2015-07-30 09:42:05 +0200

    before 'I8: Improving the Project Structure'

--- idea_box/app.rb
+++ idea_box/app.rb
@@ -1,4 +1,5 @@
 require './idea'
+require './idea_store'
 
 class IdeaBoxApp < Sinatra::Base
   set :method_override, true
@@ -8,27 +9,26 @@ class IdeaBoxApp < Sinatra::Base
   end
 
   get '/' do
-    erb :index, locals: {ideas: Idea.all}
+    erb :index, locals: {ideas: IdeaStore.all}
   end
 
   get '/:id/edit' do |id|
-    idea = Idea.find id.to_i
+    idea = IdeaStore.find id.to_i
     erb :edit, locals: {id: id, idea: idea}
   end
 
   post '/' do
-    idea = Idea.new params[:idea]
-    idea.save
+    IdeaStore.create params[:idea]
     redirect '/'
   end
 
   put '/:id' do |id|
-    Idea.update(id.to_i, params[:idea])
+    IdeaStore.update(id.to_i, params[:idea])
     redirect '/'
   end
 
   delete '/:id' do |id|
-    Idea.delete id.to_i
+    IdeaStore.delete id.to_i
     redirect '/'
   end
 end
