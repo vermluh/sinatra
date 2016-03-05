18a2235bc1285ca31b83abfa9baa4de6663ef204
Florian Simon <github@verm-luh.de>
2016-03-05 10:56:31 +0100

    Added images and layouts. Separated layout from views

---
+++ jump_start/ch02/main.rb
@@ -0,0 +1,19 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  erb :home
+end
+
+get '/about' do
+  @title = "All About This Website"
+  erb :about
+end
+
+get '/contact' do
+  erb :contact
+end
+
+not_found do
+  erb :not_found
+end
