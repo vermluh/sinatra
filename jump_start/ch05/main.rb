1fa4758c84725ac59d23a06090e0ba86f9d5f597
Florian Simon <github@verm-luh.de>
2016-03-09 15:07:52 +0100

    moved song routes to song.rb file

--- jump_start/ch05/main.rb
+++ jump_start/ch05/main.rb
@@ -78,43 +78,6 @@ post '/login' do
   end
 end
 
-get '/songs' do
-  find_songs
-  slim :songs
-end
-
-get '/songs/new' do
-  halt(401, 'Not Authorized') unless session[:admin]
-  @song = Song.new
-  slim :new_song
-end
-
-get '/songs/:id' do
-  @song = find_song
-  slim :show_song
-end
-
-get '/songs/:id/edit' do
-  @song = find_song
-  slim :edit_song
-end
-
-post '/songs' do
-  create_song
-  redirect to("/songs/#{song.id}")
-end
-
-put '/songs/:id' do
-  song = find_song
-  song.update(params[:song])
-  redirect to("/songs/#{song.id}")
-end
-
-delete '/songs/:id' do
-  find_song.destroy
-  redirect to('/songs')
-end
-
 not_found do
   slim :not_found
 end
