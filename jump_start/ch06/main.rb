bf0e55581a83fd87414f884fecb1ec5db9449338
Florian Simon <github@verm-luh.de>
2016-03-09 16:28:02 +0100

    added Ch06

---
+++ jump_start/ch06/main.rb
@@ -0,0 +1,65 @@
+# encoding: UTF-8
+
+require 'rubygems'
+require 'bundler/setup'
+
+require 'sass'
+require 'sinatra'
+require './sinatra/auth'
+require 'sinatra/flash'
+require 'sinatra/reloader' if development?
+require 'slim'
+require './song.rb'
+
+configure :development do
+  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
+  set :username, 'fez'
+  set :password, '1234'
+end
+
+configure :production do
+  #production configuration here
+end
+
+configure :test do
+  #test configuration here
+end
+
+helpers do
+  def css(*stylesheets)
+    stylesheets.map do |stylesheet|
+      "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
+    end.join
+  end
+  
+  def current?(path='/')
+    (request.path == path || request.path == path+'/') ? "current" : nil
+  end
+  
+  def set_title
+    @title ||= "Songs By Sinatra"
+  end
+end
+
+before do
+  set_title
+end
+
+get('/styles.css'){ scss :styles }
+
+get '/' do
+  slim :home
+end
+
+get '/about' do
+  @title = 'All About This Website'
+  slim :about
+end
+
+get '/contact' do
+  slim :contact
+end
+
+not_found do
+  slim :not_found
+end
