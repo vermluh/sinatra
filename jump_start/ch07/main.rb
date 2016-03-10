8bef4cd8c15d441e7483919d79bb5a15f1b5ad13
Florian Simon <github@verm-luh.de>
2016-03-10 14:53:32 +0100

    refactored to modular style application

--- jump_start/ch07/main.rb
+++ jump_start/ch07/main.rb
@@ -5,29 +5,38 @@ require 'bundler/setup'
 
 require 'coffee-script'
 require 'sass'
-require 'sinatra'
+require 'sinatra/base'
 require './sinatra/auth'
 require 'sinatra/flash'
-require 'sinatra/reloader' if development?
 require 'slim'
-require './song.rb'
 require 'v8'
 
-configure :development do
-  DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
-  set :username, 'fez'
-  set :password, '1234'
-end
+class Website < Sinatra::Base
+  register Sinatra::Auth
+  register Sinatra::Flash
+  
+  configure do
+    enable :sessions
+    set :username, 'fez'
+    set :password, '1234'
+  end
+  
+  configure :development do
+    #development configuration here
+  end
 
-configure :production do
-  #production configuration here
-end
+  configure :production do
+    #production configuration here
+  end
+  
+  configure :test do
+    #test configuration here
+  end
+  
+  before do
+    set_title
+  end
 
-configure :test do
-  #test configuration here
-end
-
-helpers do
   def css(*stylesheets)
     stylesheets.map do |stylesheet|
       "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
@@ -41,28 +50,24 @@ helpers do
   def set_title
     @title ||= "Songs By Sinatra"
   end
-end
 
-before do
-  set_title
-end
+  get('/styles.css'){ scss :styles }
+  get('/javascripts/application.js'){ coffee :application }
 
-get('/styles.css'){ scss :styles }
-get('/javascripts/application.js'){ coffee :application }
+  get '/' do
+    slim :home
+  end
 
-get '/' do
-  slim :home
-end
+  get '/about' do
+    @title = 'All About This Website'
+    slim :about
+  end
 
-get '/about' do
-  @title = 'All About This Website'
-  slim :about
-end
-
-get '/contact' do
-  slim :contact
-end
-
-not_found do
-  slim :not_found
+  get '/contact' do
+    slim :contact
+  end
+  
+  not_found do
+    slim :not_found
+  end
 end
--- jump_start/ch07/song.rb
+++ jump_start/ch07/song.rb
@@ -2,6 +2,11 @@
 
 require 'dm-core'
 require 'dm-migrations'
+require 'sass'
+require './sinatra/auth'
+require 'sinatra/base'
+require 'sinatra/flash'
+require 'slim'
 
 class Song
   include DataMapper::Resource
@@ -33,58 +38,93 @@ module SongHelpers
   end
 end
 
-helpers SongHelpers
-
-get '/songs' do
-  find_songs
-  slim :songs
-end
-
-get '/songs/new' do
-  protected!
-  @song = Song.new
-  slim :new_song
-end
-
-get '/songs/:id' do
-  @song = find_song
-  slim :show_song
-end
-
-get '/songs/:id/edit' do
-  @song = find_song
-  slim :edit_song
-end
-
-post '/songs' do
-  protected!
-  if create_song
-    flash[:notice] = "Song successfully added"
+class SongController < Sinatra::Base
+  enable :method_override
+  register Sinatra::Flash
+  register Sinatra::Auth
+  
+  helpers SongHelpers
+  
+  configure do
+    enable :sessions
+    set :username, 'fez'
+    set :password, '1234'
   end
-  redirect to("/songs/#{song.id}")
-end
-
-put '/songs/:id' do
-  protected!
-  song = find_song
-  if song.update(params[:song])
-    flash[:notice] = "Song successfully updated"
+  
+  configure :development do
+    #development configuration here
+    DataMapper.setup(:default, "sqlite3://#{Dir.pwd}/development.db")
   end
-  redirect to("/songs/#{song.id}")
-end
-
-delete '/songs/:id' do
-  protected!
-  if find_song.destroy
-    flash[:notice] = "Song deleted"
+  
+  before do
+    set_title
   end
-  redirect to('/songs')
-end
+  
+  def css(*stylesheets)
+    stylesheets.map do |stylesheet|
+      "<link href=\"/#{stylesheet}.css\" media=\"screen, projection\" rel=\"stylesheet\" />"
+    end.join
+  end
+  
+  def current?(path='/')
+    (request.path == path || request.path == path+'/') ? "current" : nil
+  end
+  
+  def set_title
+    @title ||= "Songs By Sinatra"
+  end
+  
+  get '/' do
+    find_songs
+    slim :songs
+  end
+  
+  get '/new' do
+    protected!
+    @song = Song.new
+    slim :new_song
+  end
+  
+  get '/:id' do
+    @song = find_song
+    slim :show_song
+  end
 
-post '/songs/:id/like' do
-  @song = find_song
-  @song.likes = @song.likes.next
-  @song.save
-  redirect to"/songs/#{@song.id}" unless request.xhr?
-  slim :like, :layout => false
+  get '/:id/edit' do
+    @song = find_song
+    slim :edit_song
+  end
+
+  post '/' do
+    protected!
+    if create_song
+      flash[:notice] = "Song successfully added"
+    end
+    redirect to("/#{song.id}")
+  end
+  
+  put '/:id' do
+    protected!
+    song = find_song
+    if song.update(params[:song])
+      flash[:notice] = "Song successfully updated"
+    end
+    redirect to("/#{song.id}")
+  end
+  
+  delete '/:id' do
+    protected!
+    if find_song.destroy
+      flash[:notice] = "Song deleted"
+    end
+    redirect to('/')
+  end
+  
+  post '/:id/like' do
+    @song = find_song
+    @song.likes = @song.likes.next
+    @song.save
+    redirect to"/#{@song.id}" unless request.xhr?
+    slim :like, :layout => false
+  end
 end
