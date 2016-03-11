0d4cbb930fe4490dfc613c1f0dedd3fbcad04309
Florian Simon <github@verm-luh.de>
2016-03-11 12:26:26 +0100

    Ch07: added caching support for assets

--- jump_start/ch07/asset-handler.rb
+++ jump_start/ch07/asset-handler.rb
@@ -14,10 +14,14 @@ class AssetHandler < Sinatra::Base
   
   get '/javascripts/*.js' do
     pass unless settings.coffeescript?
+    last_modified File.mtime(settings.root + '/assets/' + settings.jsdir)
+    cache_control :public, :must_revalidate
     coffee (settings.jsdir + '/' + params[:splat].first).to_sym
   end
   
   get '/*.css' do
+    last_modified File.mtime(settings.root + '/assets/' + settings.cssdir)
+    cache_control :public, :must_revalidate
     send(settings.cssengine, (settings.cssdir + '/' + params[:splat].first).to_sym)
   end
 end
