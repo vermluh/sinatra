1d1a34f32f604e37f06eaff37ee1fa36a2ff09ba
Florian Simon <github@verm-luh.de>
2018-02-10 22:15:46 +0100

    simplification
    further sorting simplification

--- ddfk/books.rb
+++ ddfk/books.rb
@@ -8,14 +8,14 @@ class Books
       CSV.foreach('./data/ddfk.csv', { :col_sep => "\t", :headers => true }).each do |row|
         books_from_file << Book.new(row)
       end
-      grouped_books = books_from_file.group_by { |book| book.number[/\D+/] }
-      grouped_books.keys.sort_by { |key| key ||= '' }.each do |key|
-        @books << grouped_books[key]
-                    .sort do |x,y|
-                      comp = (x.number.to_i <=> y.number.to_i)
-                      comp.zero? ? (x.title <=> y.title) : comp
-                    end
-      end
+      books_from_file.group_by { |book| book.number[/\D+/] }
+                     .sort_by { |key, books| key ||= '' }
+                     .each do |key, books|
+                       @books << books.sort do |x,y|
+                         comp = (x.number.to_i <=> y.number.to_i)
+                         comp.zero? ? (x.title <=> y.title) : comp
+                       end
+                     end
       @books.flatten!
     end
     @books
